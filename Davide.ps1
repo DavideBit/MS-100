3f104124ee3ec2f2691eadf4e2ea6a7e99ad46d1
DavideBit <56044755+DavideBit@users.noreply.github.com>
2023-03-24 19:08:29 +0100

    Correzioni e aggiunta location utente

--- Davide.ps1
+++ Davide.ps1
@@ -1,17 +1,20 @@
-Write-Host "`nInizio`n" -ForegroundColor Green
+Write-Host "Inizio script"
 
 # Trust repository PSGallery
+Write-Host "`nTrust repository PSGallery..."
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-Write-Host "`nTrust repository PSGallery completato`n" -ForegroundColor Green
+Write-Host "OK" -ForegroundColor Green -NoNewline
 
 # Installazione AzureAD
+Write-Host "`nInstallazione AzureAD..."
 Install-Module AzureAD
-Write-Host "`nInstallazione AzureAD completata`n" -ForegroundColor Green
+Write-Host "OK" -ForegroundColor Green -NoNewline
 
 # Connessione ad AzureAD
+Write-Host "`nConnessione ad AzureAD..."
 $modAdminCred = Get-Credential
 Connect-AzureAD -Credential $modAdminCred
-Write-Host "`nConnessione ad AzureAD completata`n" -ForegroundColor Green
+Write-Host "OK" -ForegroundColor Green -NoNewline
 
 # Definizione del dominio
 $domainName = $modAdminCred.username -replace ".*@"
@@ -22,13 +25,16 @@ $hollyPasswordProfile.Password = "User.pw1"
 $hollyPasswordProfile.ForceChangePasswordNextLogin = $false
 
 # Creazione utente Holly Dickson
-New-AzureADUser -AccountEnabled $true -DisplayName "Holly Dickson" -GivenName "Holly" -Surname "Dickson" -UserPrincipalName "Holly@$domainName" -PasswordProfile $hollyPasswordProfile -mailNickName "Holly"
-Write-Host "`nCreazione utente Holly Dickson completata`n" -ForegroundColor Green
+Write-Host "`nCreazione utente Holly Dickson..."
+New-AzureADUser -AccountEnabled $true -DisplayName "Holly Dickson" -GivenName "Holly" -Surname "Dickson" `
+-UserPrincipalName "Holly@$domainName" -PasswordProfile $hollyPasswordProfile -mailNickName "Holly" -UsageLocation "us"
+Write-Host "OK" -ForegroundColor Green -NoNewline
 
 # Assegnazione del ruolo Gloabl Admin ad Holly
+Write-Host "`nAssegnazione Global Admin ad Holly..."
 $globalAdminRoleObjectId = (Get-AzureADDirectoryRole | Where-Object {$_.DisplayName -eq "Global Administrator"}).ObjectId
 $hollyObjectId = (Get-AzureADUser -ObjectID "Holly@$domainName").ObjectId
 Add-AzureADDirectoryRoleMember -ObjectID $globalAdminRoleObjectId -RefObjectId $hollyObjectId
-Write-Host "`nAssengazione ad Holly Global Admin completata`n" -ForegroundColor Green
+Write-Host "OK" -ForegroundColor Green -NoNewline
 
-Write-Host "`nFinito`n" -ForegroundColor Green
+Write-Host "`nFinito" -ForegroundColor Green
